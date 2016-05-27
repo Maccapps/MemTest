9de41ae0ea761fad2a1dcdf704d892ce8fd3dbc8
Andy McEniry <wm@maccapps.com>
2016-05-27 18:50:19 +0100

    try to get phonegap to allow remote feeds

--- italian/verb-conjugation-test.php
+++ italian/verb-conjugation-test.php
@@ -5,6 +5,7 @@
         <title>Verb Conjugation :: Italian :: MemTest :: Maccapps</title>
         <meta name=description content="">
         <meta name=viewport content="width=device-width,initial-scale=1">
+        <meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'">
 		<link rel=stylesheet href="../global/css/reset.css">
 		<link rel=stylesheet href="../global/css/app.css"> 
 		<link rel=stylesheet href="css/app.css">       
