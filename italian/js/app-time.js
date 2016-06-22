da668af17e6a0423022d0982fae65690ec4f55c8
Andy McEniry <andy@mceniry.me.uk>
2016-06-22 07:07:48 +0100

    italian times

---
+++ italian/js/app-time.js
@@ -0,0 +1,142 @@
+/*globals $*/
+/*jslint eqeq:true plusplus:true*/
+
+var oApp = window.oApp || {};
+
+(function () {
+
+    'use strict';
+
+    oApp.ls = oApp.getLs() || {};
+   
+    oApp.italianNumbers = [
+        'zero', 'uno', 'due', 'tre', 'quattro', 'cinque', 'sei', 'sette', 'otto', 'nove',
+        'dieci', 'undici', 'dodici', 'tredici', 'quattordici', 'quindici', 'sedici', 'diciassette', 'dicoitto', 'diciannove',
+        'venti', 'ventuno', 'ventidue', 'ventitre', 'ventiquattro', 'venticinque', 'ventisei', 'ventotto', 'ventinove'
+    ];
+
+    $('body').on('keyup', '.jsTimeInput', function () {
+        var el = $(this),
+            val = el.val();
+
+        if (val.toLowerCase() === oApp.answer.toLowerCase()) {
+            el.addClass('correct');
+        } else {
+            el.removeClass('correct');
+        }
+
+    });
+
+    $('.verb-questions').on('click', 'em', function () {
+        $(this).closest('.verb-questions').find('input').each(function () {
+            var el = $(this);
+
+            el.val(oApp.answer);
+
+        });
+    });
+
+    oApp.getNewTimeObject = function () {
+
+        var time = {};
+
+        time.numbers = oApp.getNewTime();
+        time.words = oApp.getTimeWordsFromNumbers(time.numbers);
+
+        return time;
+        
+    };
+
+
+    oApp.getNewTime = function () {
+
+        var hour = oApp.rand(0, 23),
+            mins = oApp.rand(0, 59),
+            random = oApp.rand(0, 1),
+            time;
+
+        if(random === 0) {
+            mins = oApp.rand(0, 3) * 15;
+        }
+
+        hour = hour < 10 ? '0' + hour : hour;
+        mins = mins < 10 ? '0' + mins : mins;
+
+        time = hour + ':' + mins;
+
+        return time;
+        
+    };
+
+    oApp.getTimeWordsFromNumbers = function (numbers) {
+
+        var bits = numbers.split(':'),
+            hour = parseInt(bits[0], 10),
+            mins = parseInt(bits[1], 10),
+            words;
+
+        if (mins > 30) {
+            hour++;
+            if (hour == 24) {
+                hour = 0;
+            }
+        }
+        if (hour == 0) {
+            words = 'e mezzanotte';
+        }
+        if (hour == 1) {
+            words = "e l'una";
+        }
+        if (hour == 12) {
+            words = 'e mezzagiorno';
+        }
+        if (hour > 1 && hour != 12) {
+            words = 'sono le ' + oApp.italianNumbers[hour];
+        }
+
+        if (mins > 30) {
+            words += ' meno ';
+        } else {
+            if (mins != 0) {
+                words += ' e ';
+            }
+        }
+
+        if (mins == 15 || mins == 45) {
+            words += 'quarto';
+        }
+        if (mins == 30) {
+            words += 'mezzo';
+        }
+        if (mins != 15 && mins!=30 && mins != 45 && mins > 30) {
+            words += oApp.italianNumbers[(60 - mins)];
+        }
+        if (mins != 15 && mins!=30 && mins != 45 && mins < 30 && mins != 0) {
+            words += oApp.italianNumbers[mins];
+        }
+
+
+        return words;
+        
+    };
+
+    oApp.getNewItem = function () {
+
+        var time = oApp.getNewTimeObject();
+
+        $('.jsTimeDisplay').html(time.numbers);
+        $('.jsTimeInput').val('').focus();
+        oApp.answer = time.words;
+        $('.home').html(oApp.answer);
+    
+    };
+
+    $('.btnContinue').click(function () {
+        oApp.getNewItem();
+    });
+
+    oApp.init = (function () {
+        oApp.getNewItem();
+    }());
+
+}());
---
+++ italian/time.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+    <head>
+        <meta charset=utf-8>
+        <title>Verb Conjugation :: Italian :: MemTest :: Maccapps</title>
+        <meta name=description content="">
+        <meta name=viewport content="width=device-width,initial-scale=1">
+		<link rel=stylesheet href="../global/css/reset.css">
+		<link rel=stylesheet href="../global/css/app.css"> 
+		<link rel=stylesheet href="css/app.css">       
+        <script src="../global/js/jquery-2.2.2.min.js"></script>	
+    </head>
+
+    <body id="test-time">
+
+		<div id="holder">
+			<div id="deviceheader"></div>
+            
+                <div class="page active">
+                    <div class="header">
+                        <p class="back"><a href="index.html">&#x25c0;</a></p>
+                        <p class="home">Telling the Time</p>
+                    </div>
+
+                    <div class="verb-questions jsVerb">
+                        <div class="item"><label class="jsTimeDisplay">22:30</label>
+                            <div>
+                                <span class="col-pronoun"> &nbsp; </span>
+                                <input class="jsTimeInput" autofocus="autofocus" placeholder="enter thee time in italian" data-answer="sono le ventidue e mezzo"/>
+                                <em>?</em>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="btn last btnContinue"><p>CONTINUE</p></div>
+
+                </div>
+            
+			<div id="devicefooter"></div>
+		</div>
+
+        <script src="../global/js/app.js"></script>	
+        <script src="js/app.js"></script>	
+        <script src="js/app-time.js"></script>	
+    </body>
+
+</html>
